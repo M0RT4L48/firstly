94c1c8bd5f25251427d348355fcfb8d556a9af8a
M0RT4L48 <moyocristiano@live.com>
2024-02-29 09:59:26 -0600

    initial commit

---
+++ lib/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/screens/chat/wiggets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/screens/chat/wiggets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/screens/chat/wiggets/chat/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(2.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://pbs.twimg.com/media/FYPfpZtXgAACWH3.jpg:large'),
+          ),
+        ),
+        title: const Text('Amor♥'),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+        children: [
+          Expanded(child: ListView.builder(
+            itemCount: 100,
+            itemBuilder: (context, index) {
+            return (index % 2 == 0)
+            ? const HerMessageBubble()
+            : const MyMessageBubble();
+            
+          })),
+            //caja de texto
+           const MessageFieldBox(),
+        ]),
+      ),
+    );
+  }
+}
