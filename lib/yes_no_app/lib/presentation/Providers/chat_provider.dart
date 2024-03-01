5f43e72d3cbe9bb04c774ac2f1f78669b20f86d1
M0RT4L48 <moyocristiano@live.com>
2024-03-01 14:48:09 -0600

    Starting with Provider

---
+++ lib/yes_no_app/lib/presentation/Providers/chat_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier{
+  List<Message> messageList = [
+    Message(text: 'Hola', fromWho: FromWho.me),
+    Message(text: 'holi, como tas?', fromWho: FromWho.hers),
+  ];
+}
--- lib/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ lib/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/presentation/screens/chat/wiggets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/screens/chat/wiggets/chat/my_message_bubble.dart';
@@ -25,6 +27,7 @@ class ChatScreen extends StatelessWidget {
 }
 
 class _ChatView extends StatelessWidget {
+  final ChatProvider = context.watch<ChatProvider>();
   
   @override
   Widget build(BuildContext context) {
@@ -34,7 +37,7 @@ class _ChatView extends StatelessWidget {
         child: Column(
         children: [
           Expanded(child: ListView.builder(
-            itemCount: 100,
+            itemCount: ChatProvider,
             itemBuilder: (context, index) {
             return (index % 2 == 0)
             ? const HerMessageBubble()
